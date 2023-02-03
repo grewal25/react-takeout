479928ae3c328538c6d196d56bdca6843153975a
Saranjeet Singh <grewalele@gmail.com>
2023-02-03 18:49:14 -0500

    add css

--- src/pages/OrderPage/MenuView/MenuView.tsx
+++ src/pages/OrderPage/MenuView/MenuView.tsx
@@ -1,14 +1,16 @@
 import { Menu } from '../../../models/Menu';
+import { Checkout } from '../../CheckoutPage';
 import { SectionView } from './SectionView';
-
+import './SectionView.css'
 export const MenuView: React.FC<{ menus: Menu }> = ({ menus }) => {
   return (
-    <div>
-      <h1>Our Menu</h1>
+    <div className="menu-top">
+      <div><h1>Our Menu</h1>
       {menus.sectionIds.map((sectionId, sectionIdx) => {
         const section = menus.sections[sectionId];
         return <SectionView key={`${sectionIdx}-${sectionIdx}`} section={section} items={menus.items} />
-})}
+})}</div>
+  <div><Checkout /></div>
     </div>
   );
 };
